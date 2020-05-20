c8acbf978894b397af68ec12ffb2180bb2f4a69f
Francisco Vasquez <franjavi4111@gmail.com>
2020-05-19 23:30:50 -0500

    pages and components folder created and boostrap installed

---
+++ avanzada/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home (){
+    return (
+        <React.Fragment>
+            <h1>Danerys</h1>
+            <h2>Danerys</h2>
+        </React.Fragment>
+        
+    );
+}
+export default Home;
---
+++ avanzada/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './Pages/Home.jsx'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.render(<Home />, document.getElementById('root'));
